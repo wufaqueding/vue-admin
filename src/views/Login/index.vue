57a2bd43b924dbb45ae39ec68a5eea1d8be05aff
wufaqueding <504903582@qq.com>
2022-02-04 17:34:12 +0800

    构建项目、配置全局样式、路由、element

---
+++ src/views/Login/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="login">
+        这里是登陆页面
+         <el-button>默认按钮</el-button>
+    </div>
+</template>
+<script>
+export default {
+    name: 'login'
+}
+</script>
+<style lang="scss" scoped>
+#login {
+    height: 100vh;
+    background-color: #344a5f;
+}
+</style>
